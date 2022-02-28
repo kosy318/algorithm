80363388e16a9718e88d259ea9fe03b467fe64aa
kosy318 <kkosy1782@naver.com>
2022-02-28 22:00:06 +0900

    1st and 2nd solution commit

---
+++ 1. lv1 신고 결과 받기.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// string split
+vector<string> split(string str){
+    istringstream iss(str);
+    string buff;
+
+    vector<string> result;
+
+    while(getline(iss, buff, ' ')){
+        result.push_back(buff);
+    }
+    return result;
+}
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    unordered_map<string, int> id_map;
+    for(int i=0; i<id_list.size(); i++)
+        id_map.insert(pair<string, int>(id_list[i], 0));
+
+    map<string, set<string>> cnt; // 신고한 유저들의 set
+    for(string ids: report){
+        vector<string> id = split(ids); // split
+        cnt[id[1]].insert(id[0]);
+    }
+
+    for(int i=0; i<id_list.size(); i++){
+        if(cnt[id_list[i]].size() >= k){
+            for(string id: cnt[id_list[i]]){
+                id_map[id]++;
+            }
+        }
+    }
+
+    vector<int> answer;
+    for(int i=0; i<id_list.size(); i++){
+        answer.push_back(id_map[id_list[i]]);
+    }
+
+    return answer;
+}
---
+++ 2. lv1 로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    sort(lottos.begin(), lottos.end());
+    sort(win_nums.begin(), win_nums.end());
+
+    int idx = 0, cnt = 0, same = 0;
+    for(int i=0; i<6; i++){
+        if(lottos[i] == 0){
+            cnt++;
+            continue;
+        }
+        while(lottos[i] > win_nums[idx])
+            idx++;
+
+        if(lottos[i] == win_nums[idx])
+            same++;
+    }
+
+    vector<int> answer;
+    answer.push_back(same == 0 && cnt == 0 ? 6 : 7-(same+cnt));
+    answer.push_back(same == 0? 6 : 7-same);
+    return answer;
+}
