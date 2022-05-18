cd43e6a96910b9724d15d5d953c11d7999cb7283
kosy318 <kkosy1782@naver.com>
2022-05-18 21:46:09 +0900

    Update 22. lv2 멀쩡한 사각형.cpp - added solution2

--- 22. lv2 멀쩡한 사각형.cpp
+++ 22. lv2 멀쩡한 사각형.cpp
@@ -1,4 +1,5 @@
-// https://jolly-note.tistory.com/55
+// solution1 : https://jolly-note.tistory.com/55
+// solution2 : https://jolly-note.tistory.com/56
 
 using namespace std;
 
@@ -9,11 +10,20 @@ int gcd(int x, int y) {
 	return gcd(x%y, y%x);
 }
 
-long long solution(int w, int h) {
+long long solution1(int w, int h) {
 	if (w == h) return (long long)w*(h - 1);
 	int gcd_val = gcd(w, h);
 
 	int deleted = h / gcd_val + w / gcd_val - 1;
 	long long answer = (long long)w*h - deleted * gcd_val;
 	return answer;
+}
+
+
+long long solution2(int w, int h) {
+	long long answer = 0;
+	for (int i = 0; i < w; i++) {
+		answer += (int)((double)h*i / w);
+	}
+	return 2 * answer;
 }
