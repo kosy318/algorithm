480339f5e6ebfa941bb1382248495691e009fc10
kosy318 <kkosy1782@naver.com>
2022-05-24 19:44:42 +0900

    add BFS

--- 32. lv2 타겟 넘버.cpp
+++ 32. lv2 타겟 넘버.cpp
@@ -14,8 +14,25 @@ void dfs(vector<int>& numbers, int& target, int idx, int num) {
 	dfs(numbers, target, idx + 1, num + numbers[idx]);
 }
 
+void bfs(vector<int>& numbers, int& target) {
+	queue<pair<int, int>> que;
+	que.push({ numbers[0], 1 });
+	que.push({ (-1)*numbers[0], 1 });
+
+	while (!que.empty()) {
+		auto[num, idx] = que.front(); que.pop();
+		if (idx == numbers.size()) {
+			if (num == target) answer++;
+			continue;
+		}
+		que.push({ num + numbers[idx], idx + 1 });
+		que.push({ num + (-1)*numbers[idx], idx + 1 });
+	}
+}
+
 int solution(vector<int> numbers, int target) {
 	dfs(numbers, target, 0, 0);
+	// bfs(numbers, target);
 
 	return answer;
 }
