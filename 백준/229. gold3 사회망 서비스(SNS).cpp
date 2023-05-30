67cfa8be11ed5da58c3c4db434f6fbf8ddc604a0
고수연 <kosy1782@gmail.com>
2023-05-30 23:42:58 +0900

    #229: 2023-05-30 (풀이중)

---
+++ 백준/229. gold3 사회망 서비스(SNS).cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+// dp[i][0] := i번째 node가 얼리어답터가 아닌 경우 자식 중 얼리어답터인 것들의 개수의 최소
+// dp[i][1] := i번째 node가 얼리어답터인 경우 자식 중 얼리어답터인 것들의 개수의 최소
+vector<vector<int>> dp;
+vector<vector<int>> adj_list;
+
+void dfs(int cur){
+    dp[cur] = {0, 1};
+
+    if (adj_list[cur].size() == 1) return; // leaf node
+
+    for (int adj: adj_list[cur]) {
+        if(dp[adj][0] == -1){ // 자식 node
+            dfs(adj);
+            dp[cur][0] += dp[adj][1];
+            dp[cur][1] += min(dp[adj][0], dp[adj][1]);
+        }
+    }
+
+    cout << "dp[" << cur << "] " <<
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    dp = vector<vector<int>>(N + 1, vector<int>(2, -1));
+    adj_list = vector<vector<int>>(N + 1);
+
+    for (int i = 0; i < N-1; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    queue<int> que;
+    for (int i = 1; i <= N; i++) {
+        if (adj_list[i].size() == 1) {
+            que.push(i);
+        }
+    }
+
+    dfs(1);
+
+    cout << min(dp[1][0], dp[1][1]);
+}
