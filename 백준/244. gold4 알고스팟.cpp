4482ed6c193084ba96d4991cb949b74f16b8df94
고수연 <kosy1782@gmail.com>
2023-06-26 02:08:37 +0900

    #244: 2023-06-25

--- 백준/244. gold4 알고스팟.cpp
+++ 백준/244. gold4 알고스팟.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <string>
 #include <deque>
-#include <algorithm>
 
 using namespace std;
 
@@ -14,13 +13,15 @@ struct Room{
 };
 
 int N, M;
-vector<vector<int>> maze;
+vector<string> maze;
 
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
 int bfs(){
     vector<vector<int>> broke_cnt(N, vector<int>(M, 10000));
+    broke_cnt[0][0] = 0;
+
     deque<Room> deq;
     deq.push_back(Room(0, 0, 0));
 
@@ -35,14 +36,11 @@ int bfs(){
             int ny = cur.y + dy[d];
 
             if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue; // 범위 벗어남
-            if(broke_cnt[nx][ny] != 10000) continue;
 
-            if(maze[nx][ny] == 0){
-                broke_cnt[nx][ny] = min(broke_cnt[nx][ny], broke_cnt[cur.x][cur.y]);
-                deq.push_front(Room(nx, ny, cur.broke));
-            } else {
-                broke_cnt[nx][ny] = min(broke_cnt[nx][ny], broke_cnt[cur.x][cur.y] + 1);
-                deq.push_back(Room(nx, ny, cur.broke + 1));
+            if(broke_cnt[nx][ny] > cur.broke + (maze[nx][ny] - '0')) {
+                broke_cnt[nx][ny] = cur.broke + (maze[nx][ny] - '0');
+                if(maze[nx][ny] == '0') deq.push_front(Room(nx, ny, cur.broke));
+                else deq.push_back(Room(nx, ny, cur.broke + 1));
             }
         }
     }
@@ -54,16 +52,13 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> N >> M;
+    //////////// 틀린 부분 : 입력 제대로 보자...
+    cin >> M >> N;
 
-    maze = vector<vector<int>>(N, vector<int>(M));
+    maze = vector<string>(N);
 
     for (int i = 0; i < N; i++) {
-        string str;
-        cin >> str;
-        for (int j = 0; j < M; j++) {
-            maze[i][j] = str[j] - '0';
-        }
+        cin >> maze[i];
     }
 
     cout << bfs();
